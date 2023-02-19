b094a161077674a16d2cecf7e121174671ecaaea
Maryia Rachkouskaya <maryia_rachkouskaya@epam.com>
2023-02-19 18:51:18 +0200

    feat: deploy to s3 and cloudfront with serverless

--- src/components/MainLayout/components/Header.tsx
+++ src/components/MainLayout/components/Header.tsx
@@ -25,7 +25,7 @@ export default function Header() {
 
   return (
     <AppBar position="relative">
-      <Toolbar>
+      <Toolbar style={{ backgroundColor: "#9CAF88" }}>
         <Typography variant="h6" sx={{ flexGrow: 1 }}>
           <Link
             component={RouterLink}
@@ -33,7 +33,7 @@ export default function Header() {
             underline="none"
             to="/"
           >
-            My Store!
+            Maryia Rachkouskaya&apos;s Store
           </Link>
         </Typography>
 
